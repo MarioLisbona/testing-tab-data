ba0c1921f79a1db35c5b8899bb53edca5257f678
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-11-13 17:19:22 +1100

    Update google_sheets_app.py

--- google_sheets_app.py
+++ google_sheets_app.py
@@ -1,16 +1,16 @@
-from lib.utils import setup_google_sheets, check_rfi_in_row
-
-
-
+from lib.utils import setup_google_sheets, process_rfi_cells, write_grouped_rfi_to_sheet
 
 # Example usage
 def main():
     client = setup_google_sheets()
-    sheet = client.open("Testing output-MYOM-IHEAB-Testing").worksheet("Testing")  # Access the "Testing" tab
+    testing_tab = client.open("els-testing-outputs").worksheet("Testing")  # Access the "Testing" tab
+    rfi_spreadsheet_tab = client.open("els-testing-outputs").worksheet("RFI Spreadsheet")  # Access the "Testing" tab
 
-    # Check RFI in a specific row (for example, row 5)
-    # print(check_rfi_in_row(sheet, 4))  # Call the new function for row 5
-    print(check_rfi_in_row(sheet, 25))  # Call the new function for row 5
+    grouped_rfi_entries = process_rfi_cells(testing_tab)  # Call the new function for row 5
+
+    if grouped_rfi_entries:  # Check if there are any entries before writing
+        write_grouped_rfi_to_sheet(grouped_rfi_entries, rfi_spreadsheet_tab)
+
 
 if __name__ == "__main__":
     main()
